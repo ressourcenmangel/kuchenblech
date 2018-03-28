85ef245be251656a2d032e9a93222ce7efc8ed8e
Dennis Morhardt <dennis.morhardt@ressourcenmangel.de>
2018-03-28 14:58:32 +0200

    🏁 Add missing polyfills for IE

--- src/assets/javascripts/main.js
+++ src/assets/javascripts/main.js
@@ -1,5 +1,12 @@
+// Polyfills
+import '@babel/polyfill';
+import 'nodelist-foreach-polyfill';
+import 'element-closest';
+
+// 3rd party libraries
 import 'prismjs';
 
+// Components
 import './shared/collapse';
 import './components/variant';
 import './components/tabs';
